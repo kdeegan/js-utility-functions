47f147925102c991d22e68ba880886a65ac7bc68
Kevin <kevindeeg@gmail.com>
2017-11-30 18:47:33 +0000

    first function

---
+++ msToTimeStr.js
@@ -0,0 +1,20 @@
+"use strict";
+
+function msToTimeStr(milliseconds) {
+    if (!milliseconds) {
+        milliseconds = (new Date).getTime();
+    }
+    var ms = milliseconds % 1000,
+        sec = parseInt(milliseconds / 1000) % 60,
+        min = parseInt(milliseconds / (1000*60)) % 60,
+        hr = parseInt(milliseconds / (1000*60*60)) % 24;
+
+    hr = (hr < 10) ? "0" + hr : hr;
+    min = (min < 10) ? "0" + min : min;
+    sec = (sec < 10) ? "0" + sec : sec;
+    ms = (ms < 100) ? "0" + ms : ms;
+    
+    return hr + ":" + min + ":" + sec + "." + ms;
+}
+
+module.exports = msToTimeStr;
---
+++ test/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const msToTimeStr = require("../msToTimeStr");
+
+for (let i = 0; i < 10; ++i) {
+    console.log("TIME: " + msToTimeStr());
+}
